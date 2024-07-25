9ce0fcb9483db198ddc385f1db61c0663ca34782
Leornadia <leornadiajose@gmail.com>
2024-07-25 12:42:23 -0700

    5-building.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -6,18 +6,15 @@ export default class SkyHighBuilding extends Building {
     this._floors = floors;
   }
 
-  // Getter for sqft (inherited from Building)
   get sqft() {
     return this._sqft;
   }
 
-  // Getter for floors
   get floors() {
     return this._floors;
   }
 
-  // Override evacuationWarningMessage
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors.`;
   }
 }
