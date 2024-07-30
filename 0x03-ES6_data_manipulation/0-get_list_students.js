fc8cb6413be9c47613c86ff6a3edc2016f7f6e19
Leornadia <leornadiajose@gmail.com>
2024-07-30 13:06:07 -0700

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,11 @@
+// 0-get_list_students.js
 
-export default const getListStudents = () => {
+const getListStudents = () => {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName:    'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
 };
 
+export default getListStudents;
