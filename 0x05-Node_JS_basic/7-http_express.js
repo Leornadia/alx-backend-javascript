ca69e66e003e265a98927fb176fdaf9b2545eb28
Leornadia <leornadiajose@gmail.com>
2024-09-17 08:44:16 -0700

    7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,78 +1,34 @@
-// Import required modules
-import express from 'express';
-import fs from 'fs';
-import { promisify } from 'util';
-import csvParse from 'csv-parse';
+const express = require('express');
+const fs = require('fs');
 
-// Promisify readFile for asynchronous file reading
-const readFileAsync = promisify(fs.readFile);
-
-// Function to read the CSV database and return the student information
-const readDatabase = async (filePath) => {
-  const data = await readFileAsync(filePath, { encoding: 'utf8' });
-  const records = [];
-
-  await new Promise((resolve, reject) => {
-    csvParse(data, { delimiter: ',' }, (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        result.forEach((row) => {
-          if (row[0] && row[1] && row[2]) { // Avoid empty lines
-            records.push({ firstname: row[0], field: row[2] });
-          }
-        });
-        resolve();
-      }
-    });
-  });
-
-  const studentsByField = {};
-
-  records.forEach((student) => {
-    const { field, firstname } = student;
-    if (!studentsByField[field]) {
-      studentsByField[field] = [];
-    }
-    studentsByField[field].push(firstname);
-  });
-
-  return studentsByField;
-};
-
-// Initialize Express application
 const app = express();
 
-// Route for '/'
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Route for '/students'
-app.get('/students', async (req, res) => {
-  const filePath = process.argv[2]; // Get the file path passed as argument
-  try {
-    const students = await readDatabase(filePath);
+app.get('/students', (req, res) => {
+  const database = process.argv[2]; // Get the database filename from command line argument
 
-    let responseText = 'This is the list of our students\n';
-    let totalStudents = 0;
+  fs.readFile(database, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading database');
+      return;
+    }
 
-    Object.keys(students).sort().forEach((field) => {
-      const fieldStudents = students[field];
-      totalStudents += fieldStudents.length;
-      responseText += `Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}\n`;
-    });
+    const students = data.split('\n').filter(line => line.trim() !== '');
+    const csStudents = students.filter(student => student.startsWith('CS:'));
+    const sweStudents = students.filter(student => student.startsWith('SWE:'));
 
-    res.send(responseText);
-  } catch (error) {
-    res.status(500).send('Cannot load the database');
-  }
+    res.send(`This is the list of our students\n
+Number of students: ${students.length}\n
+Number of students in CS: ${csStudents.length}. List: ${csStudents.map(student => student.substring(3)).join(', ')}\n
+Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map(student => student.substring(4)).join(', ')}\n`);
+  });
 });
 
-// Listen on port 1245
+module.exports = app;
+
 app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+  console.log('Server running at http://localhost:1245/');
 });
-
-export default app;
-
