d0556224f357364d86ac18217eada520087ce1e6
Leornadia <leornadiajose@gmail.com>
2024-09-20 16:10:26 +0000

    4-payment.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,33 +1,27 @@
 const sinon = require('sinon');
-const { expect } = require('chai');
+const expect = require('chai').expect;
 const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
-describe('sendPaymentRequestToApi', function () {
-  let calculateNumberStub;
-  let consoleSpy;
-
-  beforeEach(function () {
-    // Stub Utils.calculateNumber to always return 10
-    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    // Spy on console.log
-    consoleSpy = sinon.spy(console, 'log');
-  });
-
-  afterEach(function () {
-    // Restore the original methods
-    calculateNumberStub.restore();
-    consoleSpy.restore();
-  });
-
-  it('should call Utils.calculateNumber with "SUM", 100, and 20', function () {
+describe('sendPaymentRequestToApi', () => {
+  it('should stub Utils.calculateNumber and verify console.log', () => {
+    // Create a stub for Utils.calculateNumber
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    
+    // Create a spy for console.log
+    const consoleLogSpy = sinon.spy(console, 'log');
+    
+    // Call the function
     sendPaymentRequestToApi(100, 20);
-
-    // Verify that the stub was called with correct arguments
-    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
-
-    // Verify that console.log was called with the correct message
-    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+    
+    // Verify that the stub is being called with the correct arguments
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    
+    // Verify that console.log is logging the correct message
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+    
+    // Restore the stub and spy
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
   });
 });
-
